c7e8196efbd1fc01470c28e9bda8d670c3fc1776
Misha Pelih <jscript@meta.ua>
2016-12-14 17:48:11 +0200

    add menus

--- build.md
+++ build.md
@@ -5,3 +5,8 @@
   $ rails g scaffold category name
   ## Integrate category with pages
     $ rails g migration add_category_to_pages category:references
+
+# Menus
+  $ rails g model menu name
+  $ rails g model menu_item title url menu:references
+  $ rails g controller menus index new create update edit destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
+  root 'admin/pages#index'
+
   resources :categories, only: [:show]
   resources :pages, only: [:show]
 
   namespace :admin do
     resources :pages # /admin/pages
     resources :categories
+    resources :menus, except: [:show]
   end
 
   Page.where.not("slug", nil).all.each do |page|
     get "/#{page.slug}", controller: "pages", action: "show", id: page.id
   end
 
-  root 'admin/pages#index'
+  mount Ckeditor::Engine => '/ckeditor'
 end
